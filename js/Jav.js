14bd64f2ac8c6c292b8f6034b9a9cd19f98b8611
Fernando-Anzora <101297615+Fernando-Anzora@users.noreply.github.com>
2022-08-20 00:29:32 -0600

    Update Jav.js

--- js/Jav.js
+++ js/Jav.js
@@ -1,11 +1,13 @@
 var btn1 = document.getElementById("btn1");
 var btn2 = document.getElementById("btn2");
 var btn3 = document.getElementById("btn3");
+var btn4 = document.getElementById("btn4");
 
 
 btn1.addEventListener('click',btns1,true);
 btn2.addEventListener('click',btns2,true);
 btn3.addEventListener('click',btns3,true);
+btn4.addEventListener('click',btns4,true);
 
 function btns1(){
     
@@ -20,4 +22,8 @@ function btns2(){
 function btns3(){
     
     location.href = "act3.html";
+}
+function btns4(){
+    
+    location.href = "act4.html";
 }
