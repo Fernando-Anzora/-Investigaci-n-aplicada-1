3847f11a67063210ca4948f51993c658deb6de36
Fernando-Anzora <101297615+Fernando-Anzora@users.noreply.github.com>
2022-08-19 23:18:08 -0600

    Add files via upload

---
+++ js/Jav.js
@@ -0,0 +1,23 @@
+var btn1 = document.getElementById("btn1");
+var btn2 = document.getElementById("btn2");
+var btn3 = document.getElementById("btn3");
+
+
+btn1.addEventListener('click',btns1,true);
+btn2.addEventListener('click',btns2,true);
+btn3.addEventListener('click',btns3,true);
+
+function btns1(){
+    
+    location.href = "index.html";
+}
+
+function btns2(){
+    
+    location.href = "act2.html";
+}
+
+function btns3(){
+    
+    location.href = "act3.html";
+}
